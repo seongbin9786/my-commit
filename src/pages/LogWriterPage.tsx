0d8349bbc3b2cc8e30a8ff776858f229692fa86f
Seongbin Kim <seongbin9786@gmail.com>
2025-12-25 16:54:38 +0900

    feat(theme): theme dropdown 페이지에 연동

--- src/pages/LogWriterPage.tsx
+++ src/pages/LogWriterPage.tsx
@@ -4,6 +4,7 @@ import { DayNavigator } from '../components/days/DayNavigator';
 import { TextLogContainer } from '../components/texts/TextLogContainer';
 import { Area_AvailableRestTimeChart } from '../features/AvailableRestTimeChartArea';
 import { Area_ProductivePaceChart } from '../features/ProductivePaceChartArea';
+import { ThemeSelector } from '../features/theme/ThemeSelector';
 import { RootState } from '../store';
 
 export const LogWriterPage = () => {
@@ -20,7 +21,10 @@ export const LogWriterPage = () => {
         <h1 className="text-xs font-bold sm:text-sm">
           [기록지] ({currentDate})
         </h1>
-        <DayNavigator />
+        <div className="flex items-center gap-2">
+          <DayNavigator />
+          <ThemeSelector />
+        </div>
       </div>
       <div className="my-0 grid min-h-0 flex-1 grid-cols-1 grid-rows-4 p-4 sm:grid-cols-2 sm:grid-rows-2">
         <TextLogContainer />
